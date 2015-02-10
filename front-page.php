51428426b9ab4040557dbca1f90e902e4331a336
Don Huntley <don.huntley@firethornedesign.com>
2015-02-09 16:17:59 -0800

    Added custom front page template & dummy content

--- front-page.php
+++ front-page.php
@@ -1,4 +1,52 @@
-Hello! This was the front-page.php
+<?php
+/**
+ * The custom template for the one-page style front page. Kicks in automatically.
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+                    <section id="call-to-action">
+                        <div class="front-left">
+                            <h2 class="section-title">Call to Action</h2>
+                        </div>
+                        <div class="front-right">
+                            Form
+                        </div>                    
+                    </section>
+
+                    <section id="testimonials">
+                        Testimonials
+                    </section>
+                    
+                    <section id="services">
+                        <h2 class="services">Services</h2>
+                        some text goes here
+                        <ul>
+                            <li>Item one</li>
+                            <li>Item two</li>
+                            <li>Item 3</li>                         
+                        </ul>   
+                    </section>
+                    
+                    <section id="about">
+                        <h2 class="section-title">About Us</h2>
+                    </section>
+                        
+                    <section id="contact">
+                        <div class="front-left">
+                            <h2 class="section-title">Contact Us</h2>
+                        </div>
+                        <div class="front-right">
+                            Address
+                        </div>                    
+                    </section>
+                        
+                    
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 
-This is some dummy content
+<?php get_footer(); ?>
