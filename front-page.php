7460f472c3adffcae3369be682bbfa88e75e5b84
Don Huntley <don.huntley@firethornedesign.com>
2015-02-10 11:04:40 -0800

    Added overall front-page styling

--- front-page.php
+++ front-page.php
@@ -5,45 +5,64 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area one-page">
 		<main id="main" class="site-main" role="main">
 
                     <section id="call-to-action">
-                        <div class="front-left">
-                            <h2 class="section-title">Call to Action</h2>
+                        <div class="indent">
+                            <div class="front-left">
+                                <h2 class="section-title">Call to Action</h2>
+                            </div>
+                            <div class="front-right">
+                                Form
+                            </div> 
                         </div>
-                        <div class="front-right">
-                            Form
-                        </div>                    
                     </section>
-
-                    <section id="testimonials">
-                        Testimonials
+               
+                    <section id="services">
+                        <div class="indent">
+                            <h2 class="services">Services</h2>
+                            some text goes here
+                            <ul>
+                                <li>Item one</li>
+                                <li>Item two</li>
+                                <li>Item 3</li>                         
+                            </ul>  
+                        </div>
                     </section>
                     
-                    <section id="services">
-                        <h2 class="services">Services</h2>
-                        some text goes here
-                        <ul>
-                            <li>Item one</li>
-                            <li>Item two</li>
-                            <li>Item 3</li>                         
-                        </ul>   
+                    <section id="testimonials">
+                         <div class="indent">
+                            Testimonials
+                         </div>
                     </section>
+                    
+                    <section id="portfolio">
+                         <div class="indent">
+                             <h2 class="section-title">Our Work</h2>
+                         </div>
+                    </section>
+                        
                     
                     <section id="about">
-                        <h2 class="section-title">About Us</h2>
+                        <div class="indent">
+                            <h2 class="section-title">About Us</h2>
+                        </div>
                     </section>
                         
                     <section id="contact">
-                        <div class="front-left">
-                            <h2 class="section-title">Contact Us</h2>
-                        </div>
-                        <div class="front-right">
-                            Address
-                        </div>                    
+                        <div class="indent">
+                            <div class="front-left">
+                                <h2 class="section-title">Contact Us</h2>
+                            </div>
+                            <div class="front-right">
+                                Address
+                            </div>  
+                         </div>
                     </section>
-                        
+                    
+                    
+  
                     
 		</main><!-- #main -->
 	</div><!-- #primary -->
