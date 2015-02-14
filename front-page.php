23d9eb916d10fcfed5f92554a3264e033a64d6f6
Don Huntley <don.huntley@firethornedesign.com>
2015-02-13 18:53:18 -0800

    testimonials and recent work added
    testimonials styled; recent work temporarily using testimonial styles

--- front-page.php
+++ front-page.php
@@ -29,55 +29,143 @@ get_header(); ?>
                             ?>
                         </div><!-- .indent -->
                     </section><!-- #call-to-action -->
-               
-                    <section id="services">
-                        <div class="indent clear">
-                            <?php 
-                            $query = new WP_Query( 'pagename=services' );
-                                
-                            // The Loop
-                            if ( $query->have_posts() ) {
-                                    while ( $query->have_posts() ) {
-                                            $query->the_post();
-                                            echo '<h2 class="section-title">' . get_the_title() . '</h2>';
-                                            echo '<div class="entry-content">';
-                                            the_content('');
-                                            echo '</div>';
-                                    }
-                            }
-                            /* Restore original Post Data */
-                            wp_reset_postdata();
-                            ?>
-                        </div><!-- .indent -->
-                    </section><!-- #services -->
                     
                     <section id="testimonials">
                          <div class="indent">
-                            Testimonials
-                         </div>
-                    </section>
-                    
-                    <section id="portfolio">
-                        <div class="indent clear">
-                            <?php 
-                            $query = new WP_Query( 'pagename=portfolio' );
-                            // The Loop
-                            if ( $query->have_posts() ) {
-                                    while ( $query->have_posts() ) {
-                                            $query->the_post();
-                                            echo '<h2 class="section-title">' . get_the_title() . '</h2>';
-                                            echo '<div class="entry-content">';
-                                            the_content();
-                                            echo '</div>';
-                                    }
+                             <?php 
+                                $args = array(
+                                    'posts_per_page' => 3,
+                                    'orderby' => 'rand',
+                                    'category_name' => 'testimonials'                                   
+                                );
+                                $query = new WP_query( $args );
+                                if ( $query->have_posts() ) {
+                                echo '<ul class="testimonials">';
+                                while ( $query->have_posts() ) {
+                                    $query->the_post();
+                                    echo '<li class="clear">';
+                                    echo '<div class="testimonial-thumb">';
+                                    the_post_thumbnail('testimonial-mug');
+                                    echo '</div>';
+                                    echo '<aside class="testimonial-text">';
+                                    echo '<h3 class="testimonial-name">' . get_the_title() . '</h3>';
+                                    echo '<div class="testimonial-excerpt">';
+                                    the_content('');
+                                    echo '</div>';
+                                    echo '</aside>';
+                                    echo '</li>';
+                                }
+                                echo '</ul>';
                             }
 
                             /* Restore original Post Data */
                             wp_reset_postdata();
                             ?>
+                             
+                         </div>
+                    </section>
+                    
+                    <section id="services">
+                        <div class="indent clear">
+                                <?php 
+                                $query = new WP_Query( 'pagename=services' );
+                                $services_id = $query->queried_object->ID;
+
+                                // The Loop
+                                if ( $query->have_posts() ) {
+                                        while ( $query->have_posts() ) {
+                                                $query->the_post();
+                                                echo '<h2 class="section-title">' . get_the_title() . '</h2>';
+                                                echo '<div class="entry-content">';
+                                                the_content('');
+                                                echo '</div>';
+                                        }
+                                }
+
+                                /* Restore original Post Data */
+                                wp_reset_postdata();
+
+                                $args = array(
+                                        'post_type' => 'page',
+                                        'post_parent' => $services_id,
+                                        'orderby' => 'menu_order'                                       
+                                );
+                                $services_query = new WP_Query( $args );
+
+                                // The Loop
+                                if ( $services_query->have_posts() ) {
+
+                                        echo '<ul class="services-list">';
+                                        while ( $services_query->have_posts() ) {
+                                                $services_query->the_post();
+                                                echo '<li class="clear">';
+                                                echo '<a href="' . get_permalink() . '" title="Learn more about ' . get_the_title() . '">';
+                                                echo '<h3 class="services-title">' . get_the_title() . '</h3>';
+                                                echo '</a>';
+                                                echo '<div class="services-lede">';
+                                                the_content('Read more <i class="fa fa-arrow-circle-o-right"></i>');
+                                                echo '</div>';
+                                                echo '</li>';
+                                        }
+                                        echo '</ul>';
+                                }
+
+                                /* Restore original Post Data */
+                                wp_reset_postdata();
+                                ?>
                         </div><!-- .indent -->
-                    </section><!-- #meet -->
+                    </section>
+                    
+                    <section id="portfolio">
+                         <div class="indent">
+                             <?php 
+                                
+                             $query = new WP_Query( 'pagename=portfolio' );
+                            // The Loop
+                                if ( $query->have_posts() ) {
+                                        while ( $query->have_posts() ) {
+                                                $query->the_post();
+                                                echo '<h2 class="section-title">' . get_the_title() . '</h2>';
+                                                echo '<div class="entry-content">';
+                                                the_content();
+                                                echo '</div>';
+                                        }
+                                }
+
+                                /* Restore original Post Data */
+                                wp_reset_postdata();
                         
+                                $args = array(
+                                    'posts_per_page' => 6,
+                                    'orderby' => 'none',
+                                    'category_name' => 'portfolio'                                   
+                                );
+                                $query = new WP_query( $args );
+                                if ( $query->have_posts() ) {
+                                    echo '<ul class="testimonials">';
+                                    while ( $query->have_posts() ) {
+                                        $query->the_post();
+                                        echo '<li class="clear">';
+                                        echo '<div class="testimonial-thumb">';
+                                        the_post_thumbnail('testimonial-mug');
+                                        echo '</div>';
+                                        echo '<aside class="testimonial-text">';
+                                        echo '<h3 class="testimonial-name">' . get_the_title() . '</h3>';
+                                        echo '<div class="testimonial-excerpt">';
+                                        the_content('');
+                                        echo '</div>';
+                                        echo '</aside>';
+                                        echo '</li>';
+                                    }
+                                    echo '</ul>';
+                                }
+
+                                /* Restore original Post Data */
+                                wp_reset_postdata();
+                            ?>
+                             
+                         </div>
+                    </section>
                     
                     <section id="about">
                         <div class="indent clear">
